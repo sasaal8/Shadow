e81c669cb3ebffd15a0801a116c8f8c862dede2a
sasaal8 <101926168+sasaal8@users.noreply.github.com>
2022-03-19 12:49:08 +0200

    Update and rename program/utils/inline.py to برنامج/المرافق/inline.py

--- برنامج/المرافق/inline.py
+++ برنامج/المرافق/inline.py
@@ -12,7 +12,7 @@ def stream_markup(user_id):
   buttons = [
     [
       InlineKeyboardButton(text="• الـقـائـمـه♪", callback_data=f'cbmenu | {user_id}'),
-      InlineKeyboardButton(text="• الـتـحـديـثـات♪", url=f'https://t.me/FA9SH'),
+      InlineKeyboardButton(text="• الـتـحـديـثـات♪", url=f'https://t.me/L_S_A_V_O'),
     ],
     [
     InlineKeyboardButton(
